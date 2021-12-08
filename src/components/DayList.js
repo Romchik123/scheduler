28ded5211cfe6d457f3135815cde1efe7dd4d3de
rom <mozzieisme@gmail.com>
2021-12-07 22:11:03 -0500

    Refactoring some code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,19 +2,15 @@ import React from "react";
 
 import DayListItem from "components/DayListItem";
 
-// import classNames from "classnames";
-
-// import "components/DayListItem.scss";
-
 export default function DayList(props) {
   const { days, day, setDay } = props;
 
   const parsedDayListItem = days.map((iterator) => {
     return (
       <DayListItem
-        key={days.id}
-        selected={iterator.name === day}
+        key={iterator.id}
         {...iterator}
+        selected={iterator.name === day}
         setDay={setDay}
       />
     );
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,6 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected === true,
     "day-list__item--full": props.spots === 0,
@@ -18,8 +17,7 @@ export default function DayListItem(props) {
       result = `no spots remaining`;
     } else if (params === 1) {
       result = "1 spot remaining";
-    } 
-    else if (params === 2) {
+    } else if (params === 2) {
       result = "2 spots remaining";
     } else {
       result = `${params} spots remaining`;
