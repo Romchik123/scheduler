1b8543141b49a38f0b16e386ddd478ac7474e157
rom <mozzieisme@gmail.com>
2021-12-08 16:22:27 -0500

    Modify the file

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,11 +4,24 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 import "components/InterviewerList.scss";
 
-function InterviewerList() {
+function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const parsedInterviewerListItem = interviewers.map((iterator) => {
+    return (
+      <InterviewerListItem
+        key={iterator.id}
+        {...iterator}
+        selected={iterator.id === interviewer}
+        setInterviewer={setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{parsedInterviewerListItem}</ul>
     </section>
   );
 }
