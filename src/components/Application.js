1cd0b4889a2db7421f938b59dbd692b6a60cccd8
rom <mozzieisme@gmail.com>
2021-12-09 02:25:49 -0500

    Modify your App file

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,48 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 
+import Appointment from "./Appointment";
+
+// Mock DataBase ::
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+  },
+];
+
 const days = [
   {
     id: 1,
@@ -22,8 +64,13 @@ const days = [
   },
 ];
 
+// Application Component ::
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const parsedApps = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
 
   return (
     <main className="layout">
@@ -43,9 +90,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{parsedApps}</section>
     </main>
   );
 }
