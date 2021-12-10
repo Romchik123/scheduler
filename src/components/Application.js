d3fd3be53a026692651a24a170f2d2685d35eb2a
rom <mozzieisme@gmail.com>
2021-12-10 13:44:27 -0500

    Modify the file to show the aappointments and interviewers on the front end

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,65 +8,42 @@ import DayList from "components/DayList";
 
 import Appointment from "./Appointment";
 
-// Mock DataBase ::
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "4pm",
-  },
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 // Application Component ::
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
+    appointments: {},
   });
 
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
-    const url = "/api/days";
-    axios.get(url).then((response) => {
-      setDays(response.data);
-      console.log(response.data);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      Promise.resolve(all[0]); // first
+      Promise.resolve(all[1]); // second
+      Promise.resolve(all[2]); // third
+
+      const [days, appointments, interviewers] = all;
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+      console.log(days, appointments, interviewers);
     });
   }, []);
 
-  const parsedApps = appointments.map((appointment) => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const parsedApps = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
