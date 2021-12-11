7d91f874a75d0b159e0b3172a07c52ce0e34abd0
rom <mozzieisme@gmail.com>
2021-12-11 04:04:37 -0500

    Modify files and add new files

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const useVisualMode = function (initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    setMode(mode);
+    if (!replace) {
+      setHistory((prev) => [...prev, mode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+
+      setHistory(newHistory);
+
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
