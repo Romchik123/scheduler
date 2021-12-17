8306ed8e5f391a5bc1ceba8c9aa59a3be14ceab6
rom <mozzieisme@gmail.com>
2021-12-17 15:43:21 -0500

    Modify the file

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -52,9 +52,8 @@ describe("Form", () => {
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } =
+      render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText("Save"));
 
@@ -65,10 +64,17 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" },
     });
 
+    //add interviewer
+    fireEvent.click(getByAltText("Sylvia Palmer"));
+
     fireEvent.click(getByText("Save"));
 
-    expect(onSave).toHaveBeenCalledTimes(0);
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
+
   it("calls onCancel and resets the input field", () => {
     const onCancel = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
